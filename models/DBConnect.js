056cdfa5d01b9f4429e5fa3275cd4c9ed2bf5026
aznh <maxpowergame@gmail.com>
2016-06-25 17:50:33 +0900

    Delete DBConnect.js

--- models/DBConnect.js
+++
@@ -1,7 +0,0 @@
-var S = require('sequelize');
-var sql = new S('ziptoss', 'admin', 'ziptoss0704', {
-    host : 'ziptoss.cotpo2v1rlxq.ap-northeast-2.rds.amazonaws.com',
-    port : 3306
-})
-
-module.exports = sql;
