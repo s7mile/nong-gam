964226a7f04cf363df3dce678694129f3a2a4fc4
aznh <ubuntu@ip-172-31-18-75.ap-northeast-1.compute.internal>
2016-06-29 10:16:17 +0900

    상단바 추가 & css 수정

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //port setup
-app.set('port', '8000');
+app.set('port', '8080');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
